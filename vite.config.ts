3995aa7bd7ec62e5ecec2976239d384175a39574
Majd Alnajjar <143060206+majda1najjar@users.noreply.github.com>
2025-12-09 13:10:53 -0800

    Add comment for plugins and extensions section
    Added a comment to indicate where to add plugins and extensions.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
-
+//Add plugins and extensions
   export default defineConfig({
     plugins: [react()],
     resolve: {
